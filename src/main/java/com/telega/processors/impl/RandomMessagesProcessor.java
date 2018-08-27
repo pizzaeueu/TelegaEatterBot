3eebaf3074fe7cdba57de7003cda38b04c40fd32
Artsiom.Samasadau <pizzaeueu@gmail.com>
2018-08-27 17:13:27 +0300

    simplify random messages --fix typo

--- src/main/java/com/telega/processors/impl/RandomMessagesProcessor.java
+++ src/main/java/com/telega/processors/impl/RandomMessagesProcessor.java
@@ -16,7 +16,7 @@ public class RandomMessagesProcessor implements MessageProcessor {
         messages.put("/ГруппаКрови", "На руковееее");
         messages.put("/ООО", "ПОШЛИ ВЫ ВСЕ НАХУЙ");
         messages.put("/Саня", "Ты в порядке?");
-        messages.put("/OmaeWaMouShindeiru", "Крест бля");
+        messages.put("/OmaeWaMouShindeiru", "NANIIII????!!!!!");
         messages.put("/япошутил", ")))");
 
         String rules = "";
@@ -34,25 +34,4 @@ public class RandomMessagesProcessor implements MessageProcessor {
         String result = messages.get(message);
         return result != null ? result : "Да проще тебе ебало набить";
     }
-/*        if (message.equals("/Артем")) {
-            return "Крест бля";
-        } else if (message.equals("/ГруппаКрови")) {
-            return "На руковееее";
-        } else if (message.equals("/ООО")) {
-            return "ПОШЛИ ВЫ ВСЕ НАХУЙ";
-        } else if (message.equals(BotUtils.HELP_MESSAGE)) {
-            String rules = "";
-            rules += "Сообщение " + BotUtils.START_GAME_MESSAGE + " - начинает игру \n";
-            rules += "Что бы внести свой ответ напиши - " + BotUtils.ANSWER_MESSAGE + " и цену(не забудь пробел) пример - " + BotUtils.ANSWER_MESSAGE + " 34.4 \n";
-            rules += "Когда ведущий желает закончить игру - пишет " + BotUtils.FINISH_GAME_MESSAGE + " и цену, пример - " + BotUtils.FINISH_GAME_MESSAGE + " 25.5 \n";
-            return rules;
-        } else if (message.equals("/Саня")) {
-            return "Ты в порядке?";
-        } else if (message.equals("/OmaeWaMouShindeiru")) {
-            return "NANIIII????!!!!!";
-        } else if (message.equals("/япошутил")) {
-            return ")))";
-        }
-        return "Да проще тебе ебало набить";
-    }*/
 }
